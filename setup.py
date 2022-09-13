4c12ea015e898a43ccdd3daf89f45d50d83427de
Dan Fornika <dfornika@gmail.com>
2022-09-13 16:44:50 -0700

    Tag as alpha

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_namespace_packages
 
 setup(
     name='auto-fastq-symlink',
-    version='0.1.0',
+    version='0.1.0-alpha',
     packages=find_namespace_packages(),
     entry_points={
         "console_scripts": [
